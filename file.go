e38bcba58e1f84368d8aeeed32ffb39a9805a962
kuangzhiqiang <kuangzhiqiang@xiaomi.com>
2019-10-14 17:15:00 +0800

    Substr

--- file.go
+++ file.go
@@ -14,15 +14,6 @@ func FileExists(name string) bool {
 	return true
 }
 
-func substr(s string, pos, length int) string {
-	runes := []rune(s)
-	l := pos + length
-	if l > len(runes) {
-		l = len(runes)
-	}
-	return string(runes[pos:l])
-}
-
 func ParentDirectory(directory string) string {
-	return substr(directory, 0, strings.LastIndex(directory, string(os.PathSeparator)))
+	return Substr(directory, 0, strings.LastIndex(directory, string(os.PathSeparator)))
 }
